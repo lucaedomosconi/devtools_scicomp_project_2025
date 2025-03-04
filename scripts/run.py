01c9a5d554d41c5817e67c815b6a2b1b79d7d75b
lmosconi <lmosconi@sissa.it>
2025-03-04 17:04:10 +0100

    added cpp impl of AT, matrix multipl with mpi4py

--- scripts/run.py
+++ scripts/run.py
@@ -1,19 +1,50 @@
 
-import matmul.matmult_cc as matmult
+import matmul.matmult as matmult
 import numpy as np
 import time
+import os
+from mpi4py import MPI
 
-dim = 1000
-# Generate two random dimxdim matrices
-A = np.random.rand(dim, dim)
-B = np.random.rand(dim, dim)
-time1 = time.time()
-C1 = A@B
-time2 = time.time()
-print("Time taken by the @ operator:", time2-time1)
-time1 = time.time()
-C = matmult.matmult_cc(A, B)
-time2 = time.time()
-print("Time taken by the Python function:", time2-time1)
+# Set the number of threads
+os.environ["OMP_NUM_THREADS"] = "8"
+os.environ["OPENBLAS_NUM_THREADS"] = "8"
+os.environ["NUMEXPR_NUM_THREADS"] = "8"
+# os.environ["MKL_NUM_THREADS"] = "8"
 
-# print("Difference between the two methods:", np.linalg.norm(C-C1))
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+n_rows_A = 4096
+n_cols_A = 4096
+n_rows_B = n_cols_A
+n_cols_B = 4096
+A = None
+B = None
+C = None
+# Generate two random matrices A and B with specified dimensions
+if rank == 0:
+    A = np.random.rand(n_rows_A, n_cols_A)
+    B = np.random.rand(n_rows_B, n_cols_B)
+
+
+
+time1 = time.time()
+for i in range(1):
+    C = matmult.matrixMultiply(A, B, comm, rank, size)
+time2 = time.time()
+if rank == 0:
+    print("Time taken by the Python function:", time2-time1)
+
+
+time1 = time.time()
+if rank == 0:
+    C1 = A@B
+time2 = time.time()
+if rank == 0:
+    print("Time taken by the @ operator:", time2-time1)
+
+
+
+if rank == 0:
+    print("Difference between the two methods:", np.linalg.norm(C-C1))
--- src/matmul/matmult.py
+++ src/matmul/matmult.py
@@ -1,25 +1,82 @@
 import numpy as np
 import numba
-from numba.pycc import CC
-
-cc = CC('matmult_cc')
-
-
+from mpi4py import MPI
+from memory_profiler import profile
+# import cProfile
 
 
-@cc.export('matmult_cc', 'f8[:,:](f8[:,:], f8[:,:])')
-@numba.njit(parallel=True)
-def matmult(A: np.ndarray, B: np.ndarray) -> np.ndarray:
-    rows_A, cols_A = A.shape
-    rows_B, cols_B = B.shape
-    result = np.zeros((rows_A, cols_B))
-    for i in numba.prange(rows_A):
-        for j in range(cols_B):
-            for k in range(cols_A):
-                result[i, j] += A[i, k] * B[k, j]
+# @numba.jit(nopython=True)
+def sub_matrixMultiply(A, B, size, rank):
+    n_rows_A, n_cols_A = A.shape
+    n_rows_B, n_cols_B = B.shape
+    if n_cols_A != n_rows_B:
+        raise ValueError("Inner dimensions of matrices don't match.")
+    n_rows_C = n_rows_A
+    n_cols_C = n_cols_B
+    C = np.zeros((n_rows_C, n_cols_C))
+    for i in range(rank, n_rows_A, size):
+        for j in range(n_cols_B):
+            for k in range(n_cols_A):
+                C[i, j] += A[i, k] * B[k, j]
+    return C
+
+
+@profile
+def matrixMultiply(A_in, B_in, comm, rank, size):
+    """
+    Multiply two matrices A and B using the naive method.
+    """
+    if rank == 0:
+        print("Multiplying two matrices using MPI.")
+        if A_in is None or B_in is None:
+            raise ValueError("Input matrices must not be None.")
+        if not isinstance(A_in, np.ndarray) or not isinstance(B_in, np.ndarray):
+            raise ValueError("Input matrices must be numpy arrays.")
+        if A_in.ndim != 2 or B_in.ndim != 2:
+            raise ValueError("Input matrices must be 2D.")
+        if A_in.shape[1] != B_in.shape[0]:
+            raise ValueError("Inner dimensions of matrices don't match.")
+        if comm is None:
+            raise ValueError("MPI communicator must not be None.")
+        if not isinstance(comm, MPI.Comm):
+            raise ValueError("MPI communicator must be of type MPI.Comm.")
+        if size < 1:
+            raise ValueError("Number of processes must be at least 1.")
+        if A_in.flags['C_CONTIGUOUS'] == False:
+            print("Matrix A is not C contiguous. Will create a copy.")
+            A_in = A_in.ascontiguousarray()
+        if B_in.flags['C_CONTIGUOUS'] == False:
+            print("Matrix B is not C contiguous. Will create a copy.")
+            B_in = B_in.ascontiguousarray()
+        rows_A, cols_A = A_in.shape
+        rows_B, cols_B = B_in.shape
+    else:
+        A_in = None
+        B_in = None
+        rows_A = cols_A = rows_B = cols_B = None
     
-    return result
+    rows_A = comm.bcast(rows_A, root=0)
+    cols_A = comm.bcast(cols_A, root=0)
+    rows_B = comm.bcast(rows_B, root=0)
+    cols_B = comm.bcast(cols_B, root=0)
 
-if __name__ == "__main__":
-    cc.compile()
-
+    A_local_rows = np.zeros(size, dtype=int)
+    A_local_rows[:] = rows_A // size
+    extra_rows = rows_A % size
+    A_local_rows[:extra_rows] += 1
+    
+    A_loc = np.zeros((A_local_rows[rank], cols_A))
+    sendcounts = A_local_rows*cols_A
+    senddispls = np.insert(np.cumsum(sendcounts), 0, 0)[:-1]
+    comm.Scatterv([A_in, sendcounts, senddispls, MPI.DOUBLE], A_loc, root=0)
+    C_loc = np.zeros((A_local_rows[rank], cols_B))
+    B = comm.bcast(B_in, root=0)
+    C_loc = A_loc @ B
+    C = None
+    if rank == 0:
+        C = np.zeros((rows_A, cols_B))
+    recvcounts = A_local_rows * cols_B
+    recvdispls = np.insert(np.cumsum(recvcounts), 0, 0)[:-1]
+    comm.Gatherv(C_loc, [C, recvcounts, recvdispls, MPI.DOUBLE], root=0)
+    
+    return C
